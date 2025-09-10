ec4c27b78ba49bda6e1396fd34d4d1240375a627
Andre <andremateus.2001@hotmail.com>
2025-09-10 16:40:25 -0300

    ja finalizado o sitema models e explicando cada funcao

--- zssn_project/sobrevivente/models.py
+++ zssn_project/sobrevivente/models.py
@@ -22,6 +22,38 @@ class Item(models.Model):
     """
     aqui irei criar como o sistema de Item deve seguir
     """
+    nome = models.CharField(max_length=50, unique=True)
+    pontos = models.IntegerField()
+    def __str__(self):
+        return f"{self.nome} ({self.pontos} pontos)"
 
 
+class Inventario(models.Model):
+
+    """
+    criar sistema de Inventario ligado ao sobrevivente
+    """
+    denunciante = models.ForeignKey(Sobrevivente, on_delete=models.CASCADE , related_name='denuncias_feitas')
+    denunciado = models.ForeignKey(Sobrevivente, on_delete=models.CASCADE , related_name='denuncias_recebidas')
+
+    class Meta:
+        """
+        garante que seja feito uma unica vez a deunincia gracas a funcao unique_together
+        """
+        unique_together = ('denunciante', 'denunciado')
+
+    def __str__(self):
+        return f"{self.denunciante.nome} denunciou {self.denunciado.nome}"
+
+    """
+    Usando os campos do Django (CharField, IntegerField, etc.) para poder definir as colunas no banco de dados criado
+
+    ForeignKey cria uma relação entre duas tabelas.
+
+    on_delete=models.CASCADE significa que se um sobrevivente for deletado, todos os seus itens de inventário e denúncias também serão.
+
+    related_name nos ajuda a fazer buscas inversas .
+
+    unique_together cria uma restrição para evitar dados duplicados.
+    """
 
