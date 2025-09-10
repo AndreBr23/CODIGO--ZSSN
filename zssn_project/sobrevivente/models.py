df2eb589070da057bf6023fc777e7f0c048e0097
Andre <andremateus.2001@hotmail.com>
2025-09-10 16:31:54 -0300

    ja feito a classe sobrevivente iriei inicar a classe item

--- zssn_project/sobrevivente/models.py
+++ zssn_project/sobrevivente/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+class Sobrevivente(models.Model):
+
+    """
+     irei definiar os dados do sobrevivente
+    """
+
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    sexo = models.CharField(max_length=1, choices=(('M', 'Masculino'), ('F', 'Feminino')))
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    infectado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Item(models.Model):
+
+    """
+    aqui irei criar como o sistema de Item deve seguir
+    """
+
+
 
