726753a803293d946466ca71190f10f310c1eea8
Andre <andremateus.2001@hotmail.com>
2025-09-10 16:26:26 -0300

    ja feito a conexao e criacao da api sobreveviente ao Postegres

--- zssn_project/zssn_project/settings.py
+++ zssn_project/zssn_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'zssn_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zssn_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
