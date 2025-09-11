a8926739d9df88eed916ee67c4f1acdff2df73b8
Andre <andremateus.2001@hotmail.com>
2025-09-10 21:19:44 -0300

    projeto finalizado e fazendo coreecoes de bug

--- zssn_project/sobrevivente/admin.py
+++ zssn_project/sobrevivente/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Sobrevivente, Item, Inventario, DenunciaInfeccao
 
+class InventarioInline(admin.TabularInline):
+    model = Inventario
+    extra = 1
+
+@admin.register(Sobrevivente)
+class SobreviventeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'idade', 'sexo', 'latitude', 'longitude', 'infectado')
+    list_filter = ('infectado', 'sexo')
+    search_fields = ('nome',)
+    inlines = [InventarioInline]
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'pontos')
+@admin.register(DenunciaInfeccao)
+class DenunciaInfeccaoAdmin(admin.ModelAdmin):
+    list_dsplay = ('denunciante', 'denunciado')
+    autocomplete_fields = ('denunciante', 'denunciado')
 # Register your models here.
